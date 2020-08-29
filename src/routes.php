4a1813903b0358577963e610ab1eb0d6ee749d7a
Arman Ahmadi <armanahmadidev@gmail.com>
2020-08-29 16:37:06 +0430

    wip

--- src/routes.php
+++ src/routes.php
@@ -3,4 +3,4 @@
 use Armancodes\DownloadLink\Http\Controllers\DownloadLinkController;
 use Illuminate\Support\Facades\Route;
 
-Route::get(config('download-link.download_route') . '/{link}', [DownloadLinkController::class, 'download']);
+Route::get(config('download-link.download_route') . '/{link}', [DownloadLinkController::class, 'download'])->name('download-link.download-route');
